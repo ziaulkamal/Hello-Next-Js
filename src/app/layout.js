aeed5a21d1617aa70ea93e0b83b3e70a9c1481e2
Ziaul Kamal <codepad@usa.com>
2024-08-08 12:23:02 +0700

    update Speed Insight

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,17 +9,12 @@ import '../../public/css/style.css';
 import '../../public/css/style.css';
 import Footer from './components/Footer';
 import MyScript from './components/MyScript';
-import CTASection from './components/CTASection';
-import BlogSection from './components/BlogSection';
-import FAQSection from './components/FAQSection';
-import StackTechnology from './components/StackTechnology';
-import AboutCase from './components/AboutCase';
-import ServiceSection from './components/ServiceSection';
-import PartnerSection from './components/PartnerSection';
-import HeroSection from './components/HeroSection';
+
 import Link from 'next/link';
 import Preloader from './components/Preloader';
 import Heads from './components/Heads';
+
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata = {
   title: 'Create Next App',
@@ -49,14 +44,7 @@ export default function RootLayout({ children }) {
         <Heads />
         <main>
           { children }
-        <HeroSection />
-        <PartnerSection />
-        <ServiceSection />
-        <AboutCase />
-        <StackTechnology />
-        <FAQSection />
-        <BlogSection />
-        <CTASection />
+          <SpeedInsights />
         </main>
 
       <Footer />
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,24 @@
 
+import CTASection from './components/CTASection';
+import BlogSection from './components/BlogSection';
+import FAQSection from './components/FAQSection';
+import StackTechnology from './components/StackTechnology';
+import AboutCase from './components/AboutCase';
+import ServiceSection from './components/ServiceSection';
+import PartnerSection from './components/PartnerSection';
+import HeroSection from './components/HeroSection';
 
 export default function Home() {
   return (
-    <></>
+    <>
+        <HeroSection />
+        <PartnerSection />
+        <ServiceSection />
+        <AboutCase />
+        <StackTechnology />
+        <FAQSection />
+        <BlogSection />
+        <CTASection />
+    </>
   );
 }
