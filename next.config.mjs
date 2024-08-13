9930322947a388c6fab61079370ec105863f23e4
Ziaul Kamal <codepad@usa.com>
2024-08-13 15:14:44 +0700

    enhance Performence

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     nextScriptWorkers: true,
   },
   images: {
-    domains: ['*.gstatic.com', 'upload.wikimedia.org'],domains: [
+    domains: [
       't0.gstatic.com', 't1.gstatic.com', 't2.gstatic.com', 't3.gstatic.com', 't4.gstatic.com',
       't5.gstatic.com', 't6.gstatic.com', 't7.gstatic.com', 't8.gstatic.com', 't9.gstatic.com',
       't10.gstatic.com', 't11.gstatic.com', 't12.gstatic.com', 't13.gstatic.com', 't14.gstatic.com',
